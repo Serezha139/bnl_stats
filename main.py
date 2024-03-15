92698c579c94dafad72df2c8501acb7f6c5ca152
givanov <givanov@lyte.com>
2024-03-15 19:41:41 +0100

    add all the ratings

--- main.py
+++ main.py
@@ -20,9 +20,25 @@ TEAM_POINTS_MAP = {
     8: 2,
 }
 
+SMALL_POINTS_MAP = {
+    1: 13,
+    2: 10,
+    3: 8,
+    4: 6,
+    5: 5,
+    6: 4,
+    7: 3,
+    8: 2,
+    9: 1,
+}
+
 def calculate_total_points(group):
     total_points = sum(PLAYER_POINTS_MAP.get(place, 0) for place in group['rank'])
-    return pd.Series({'total_points': total_points})
+    return pd.Series({'total_points': total_points, 'team': group['name'].values[0]})
+
+def calculate_total_points_sub(group):
+    total_points = sum(SMALL_POINTS_MAP.get(place, 0) for place in group['place'])
+    return pd.Series({'total_points': total_points, 'team': group['name'].values[0]})
 
 def calculate_total_team_points(group):
     total_points = sum(TEAM_POINTS_MAP.get(place, 0) for place in group['rank'])
@@ -56,15 +72,40 @@ tournament_results_df = pd.DataFrame(tournament_results)
 player_results_df = pd.DataFrame(player_results)
 
 full_players_results_df = player_results_df.merge(player_df, left_on='player', right_on='id')
+full_players_results_df = full_players_results_df.merge(team_df, left_on='team_y', right_on='id')
 full_teams_results_df = tournament_results_df.merge(team_df, left_on='team', right_on='id')
+
+young_players_result = full_players_results_df[full_players_results_df['is_youngster']].loc[:, ['username', 'tournament', 'score', 'rank', 'name']]
+woman_players_result = full_players_results_df[full_players_results_df['is_woman']].loc[:, ['username', 'tournament', 'score', 'rank', 'name']]
+
+young_players_result['place'] = young_players_result.groupby('tournament')['rank'].rank(method='dense')
+woman_players_result['place'] = woman_players_result.groupby('tournament')['rank'].rank(method='dense')
+
 
 player_season_points = full_players_results_df.groupby('username').apply(calculate_total_points).sort_values('total_points', ascending=False).reset_index()
 team_season_points = full_teams_results_df.groupby('name').apply(calculate_total_team_points).sort_values('total_points', ascending=False).reset_index()
+young_players_points = young_players_result.groupby('username').apply(calculate_total_points_sub).sort_values('total_points', ascending=False).reset_index()
+woman_players_points = woman_players_result.groupby('username').apply(calculate_total_points_sub).sort_values('total_points', ascending=False).reset_index()
 
-st.title('Командный зачет 4 сезона')
-st.dataframe(team_season_points, hide_index=True)
-st.title('Индивидуальный зачет 4 сезона')
-st.dataframe(player_season_points, hide_index=True)
+
+
+
+player_season_points.index = player_season_points.index + 1
+team_season_points.index = team_season_points.index + 1
+young_players_points.index = young_players_points.index + 1
+woman_players_points.index = woman_players_points.index + 1
+
+
+st.title('БНЛ Весна 2024')
+st.title('Командный зачет')
+st.dataframe(team_season_points, width=1000)
+st.title('Индивидуальный зачет')
+st.dataframe(player_season_points)
+st.title('Юношеский зачет')
+st.dataframe(young_players_points, width=800)
+st.title('Женский зачет')
+st.dataframe(woman_players_points, width=800)
+
 
 
 
